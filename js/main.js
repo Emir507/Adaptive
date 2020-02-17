3e7fe68a8c161fd597b67c7dd6088d3ec19d3448
Emir <dzhetybaev19emir@gmail.com>
2020-02-17 18:52:43 +0600

    добавил шрифты, индикаторы карусели в 3ью секцию, поменял одну картинку в 1ой секции

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-let menu = document.getElementById("sidebar");
-// menu.addEventListener("click", (e) => {
-//   e.stopPropagation;
-//   let target = e.target;
-//   target.classList.remove("nav-link--active");
-// });
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+let slides = $('#portfolio .carousel-item');
+console.log(slides);
+for (let i = 0; i < slides.length; i++) {
+  if (slides[0].classList.contains("active")) {
+    console.log('активный класс у 1го элемента');
+  }
+}
+
+$('#portfolio .carousel').carousel({
+  wrap: false,
+  interval: false
+})
+
+let navbarHeight = $('.navbar').css('height');
+let portfolioHeight = $('#portfolio').css('height');
+let firstPageHeight = Number(portfolioHeight.replace("px", "")) - Number(navbarHeight.replace('px', ''));
+console.log(firstPageHeight);
+$('#portfolio').css('height', firstPageHeight);
+
+
 $(document).ready(function () {
   $("#sidebar").mCustomScrollbar({
     theme: "minimal"
@@ -26,25 +37,24 @@ $(document).ready(function () {
     $('a[aria-expanded=true]').attr('aria-expanded', 'false');
   });
 
-  var height = $('.navbar').css('height');
-  var replaced = Number(height.replace('px', ''));
-  
+  var replaced = Number(navbarHeight.replace('px', ''));
+
+
   $("#sidebar").on("click", "a", function (event) {
     event.preventDefault();
     let target = event.target;
-    for (let i = 0; i < $("#sidebar a").length; i++) {
-      $("#sidebar a").removeClass("nav-link--active");
-    }
-    target.classList.remove("nav-link--active");
-    target.classList.add("nav-link--active");
+    // for (let i = 0; i < $("#sidebar a").length; i++) {
+    //   $("#sidebar a").removeClass("nav-link--active");
+    // }
+    // target.classList.remove("nav-link--active");
+    // target.classList.add("nav-link--active");
     $("#sidebar").removeClass("active");
     $(".overlay").removeClass("active");
     console.log(target.classList);
     var id = $(this).attr('href'),
       top = $(id).offset().top - replaced;
     $('body,html').animate({
-        scrollTop: top
-      }, 1000
-    );
+      scrollTop: top
+    }, 1000);
   });
 });
